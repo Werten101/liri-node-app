9bf1b0661d130708394c05a910b581205f44d128
Werten101 <alexanderkrikunov1@gmail.com>
2017-08-01 01:30:03 -0400

    almost

---
+++ LIRI/keys.js
@@ -0,0 +1,7 @@
+console.log("Keys loaded");
+exports.twitterKeys = {
+  consumer_key: "AiywuQbQ2iZMRsOrb1rV38eAG",
+  consumer_secret: "6rU9Kpm0yK1BDrFwQmjbrDe7yuHcdJbfiIuSvdpEdm1xiM6kz9",
+  access_token_key: "892252892563988481-nt4C5woT9XDqntHFCrrdJkEhwb9TwTS",
+  access_token_secret: "6ucGPJ8PpflgJVXTXaW4o6U8ViXVqCAOQgdeVoPdgP0RO"
+};
